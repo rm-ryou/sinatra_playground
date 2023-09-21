2d15b7e3a6134844ed93c46452909651d27b12aa
Ryo Moriya <ryo.moriya@satori.team>
2023-09-22 00:44:52 +0900

    add todos controller

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,29 @@
+class TodosController < ApplicationController
+  # GET /tasks
+  get '/todos' do
+  end
+
+  # GET /tasks/1
+  get 'todos/:id' do
+  end
+
+  # GET /tasks/new
+  get 'tasks/new' do
+  end
+
+  # GET /tasks/1/edit
+  get '/tasks/:id/edit' do
+  end
+
+  # POST /tasks
+  post '/tasks' do
+  end
+
+  # PUT /tasks/1
+  put '/tasks/:id' do
+  end
+
+  # DELETE /tasks/:id
+  delete '/tasks/:id' do
+  end
+end
