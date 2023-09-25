0e500cd258f87338a8918245517d0199b99852e7
Ryo Moriya <ryo.moriya@satori.team>
2023-09-25 16:35:32 +0900

    add public dir

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set     :views, 'app/views'
+    set     :public_folder, 'public'
     enable  :method_override
     enable  :sessions
   end
