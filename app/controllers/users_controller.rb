3dcca632df50463e0f2b893c2f5a9bb0c12b47e8
Ryo Moriya <ryo.moriya@satori.team>
2023-09-26 11:59:16 +0900

    add errors messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,14 @@ class UsersController < ApplicationController
 
   post '/signup' do
     @user = User.new(name: params[:user][:name], password: params[:user][:password])
+    flash[:warn] = @user.errors.full_messages unless @user.valid?
 
     # if @user.save
     if params[:user][:password] == params[:user][:password_confirmation] && @user.save
       session[:user_id] = @user.id
       redirect '/todos'
     else
-      flash[:blah] = "Can't create account."
+      flash[:warn] = "password is invalid."
       redirect '/signup'
     end
   end
