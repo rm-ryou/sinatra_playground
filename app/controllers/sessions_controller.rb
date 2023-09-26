75fa64d3f76ce37a12f904cec20b8d639314de05
Ryo Moriya <ryo.moriya@satori.team>
2023-09-26 14:26:47 +0900

    add flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,13 @@ class SessionsController < ApplicationController
 
   post '/login' do
     user = User.find_by(name: params[:sessions][:name])
-    redirect '/login' if user.nil?
-    session[:user_id] = user.id
-    redirect '/todos'
+    if user.nil?
+      flash[:warn] = "invalid user or missing password"
+      redirect '/login'
+    else
+      session[:user_id] = user.id
+      redirect '/todos'
+    end
   end
 
   get '/logout' do
